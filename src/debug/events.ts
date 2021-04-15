961b09ba93a43f92791b5ca45c4d9169c7ff1c8a
Sebastian Döll <sebastian@katallaxie.me>
2021-04-15 17:14:41 +0200

    wip: define all needed events

--- src/debug/events.ts
+++ src/debug/events.ts
@@ -45,3 +45,5 @@ export class ByebugConnected {}
 export class ByebugDisconnected {}
 export class ByebugEnterPrompt {}
 export class ByebugExitPrompt {}
+export class ByebugSent {}
+export class ByebugReceived {}
